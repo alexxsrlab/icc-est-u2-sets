ba454eec95d8c2975acb0dee7893d54935882406
alexxsrlab <arielbadillo2007@gmail.com>
2025-07-01 21:33:37 -0500

    Acabeeee

---
+++ src/App.java
@@ -0,0 +1,57 @@
+import java.util.Set;
+
+import Controllers.Sets;
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        Sets set = new Sets();
+        System.out.println("--ARIEL BADILLO--");
+        System.out.println("--IMPRIMIMOS USANDO HASHSET--");
+        runHashSet(set);
+        System.out.println("--IMPRIMIMOS USANDO LINKEDHASHSET--");
+        runLinkedHashSet(set);
+        System.out.println("--IMPRIMIMOS USANDO TREESET(menos a mayor)--");
+        runTreeSet(set);
+        System.out.println("--IMPRIMIMOS USANDO TREESET EMPLEANDO UN COMPARADOR SEGUN LA LONGUITUD DE LA PALABRA DE MENOR A MAYOR--");
+        runTreeSetConComparador(set);
+        System.out.println("--IMPRIMIMOS USANDO TREESET EMPLEANDO UN COMPARADOR SEGUN LA LONGUITUD DE LA PALABRA DE MAYOR A MENOR--");
+        runTreeSetConComparadorMayor(set);
+    }
+
+    public static void runHashSet(Sets sets) {
+        Sets palabrasSet = new Sets();
+        Set<String> palabras = palabrasSet.construirHashSet();
+        for (String palabrasImp : palabras) {
+            System.out.println("-" + palabrasImp);
+        }
+    }
+    public static void runLinkedHashSet(Sets sets) {
+        Sets palabrasSet = new Sets();
+        Set<String> palabras = palabrasSet.construirLinkedHashSet();
+        for (String palabrasImp : palabras) {
+            System.out.println("-" + palabrasImp);
+        }
+    }
+    public static void runTreeSet(Sets sets) {
+        Sets palabrasSet = new Sets();
+        Set<String> palabras = palabrasSet.construirTreeSet();
+        for (String palabrasImp : palabras) {
+            System.out.println("-" + palabrasImp);
+        }
+    }
+    public static void runTreeSetConComparador(Sets sets) {
+        Sets palabrasSet = new Sets();
+        Set<String> palabras = palabrasSet.construirTreeSetConComparador();
+        for (String palabrasImp : palabras) {
+            System.out.println("-" + palabrasImp);
+        }
+    }
+    public static void runTreeSetConComparadorMayor(Sets sets) {
+        Sets palabrasSet = new Sets();
+        Set<String> palabras = palabrasSet.construirTreeSetConComparadorMayor();
+        for (String palabrasImp : palabras) {
+            System.out.println("-" + palabrasImp);
+        }
+    }
+}
